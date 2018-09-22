8822da16c22796a46fafc3497410b104012015c5
Azmath Rahiman <>
2018-09-22 19:45:07 -0400

    commited

--- Swift App/ViewController.swift
+++ Swift App/ViewController.swift
@@ -10,11 +10,37 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var Label: UILabel!
+    var tapcount = 0
+
+   
+    
+   
+    @IBAction func buttontap(_ sender: Any) {
+        
+         tapcount = tapcount + 1
+        
+        if tapcount >= 5
+    { Label.text = "You tapped 5 times!"
+        
+        }
+    }
+    
+    // something's happening here
+    /* ddsds
+ sdsd
+ sds
+ */
+    
+    @IBAction func Buttontap2(_ sender: Any) {Label.text = "Buttons are Cool!"
+         print("Button tapped")
+        
+    }
+   
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
-    }
 
 
+         }
 }
-
